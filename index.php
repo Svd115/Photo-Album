32ae942b659dc0868656b43545fee81fff58df40
Svd115 <svd115@yahoo.fr>
2018-04-15 22:29:41 +0200

    db

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 	$db = parse_url(getenv("DATABASE_URL"));
 	
-	try {$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+	try {$bdd->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
 	   array(
 		'pdo.server' => array(
 		   'driver'   => 'pgsql',
-		   'user' => $dbopts["user"],
-		   'password' => $dbopts["pass"],
-		   'host' => $dbopts["host"],
-		   'port' => $dbopts["port"],
-		   'dbname' => ltrim($dbopts["path"],'/')
+		   'user' => $db["user"],
+		   'password' => $db["pass"],
+		   'host' => $db["host"],
+		   'port' => $db["port"],
+		   'dbname' => ltrim($db["path"],'/')
 		   )
 	   )
 	);}
